9a1993e754d8fedbbc52ebd97053259a6ae26171
Basirat Hamayoon <basirathumayun@gmail.com>
2025-05-31 11:47:41 +0500

    Create server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const connectDB = require('./Config/db.js');
 require('dotenv').config();
+
+//mport routes there when created
+
+
+//database connection
+connectDB()
+
+const app = express();
+app.use(express.json());
+
+//All routes app middleware there
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`http://localhost:${port} is now running`));
