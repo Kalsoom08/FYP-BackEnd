87712fe484fdd4e27b3f145f78f87a90beb11d9f
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-01 16:02:33 +0500

    Controller is created for admin authentication

---
+++ Controllers/admin/authController.js
@@ -0,0 +1,36 @@
+const Admin = require('../../Models/authModel');
+const comparePassword = require('../../Utils/comparePassword');
+const generateToken = require('../../Utils/JWTGenerator');
+
+
+exports.loginAdmin = async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+
+       
+        const admin = await Admin.findOne({ userName });
+        if (!admin) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const isMatch = await comparePassword(password, admin.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        
+        const token = generateToken(admin);
+        res.json({
+            message: 'Login successful',
+            token,
+            admin: {
+                id: admin._id,
+                userName: admin.userName,
+                role: admin.role
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: 'Login failed', error: err.message });
+    }
+};
