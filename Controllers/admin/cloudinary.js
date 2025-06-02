1e465b2173d82d793f1a5f4cbeec839791d85938
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-02 11:24:46 +0500

    Controller , Middleware , Router is created for file upload using cloudinary

---
+++ Controllers/admin/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require('../../Config/cloudinary');
+const catchAsync = require('../../Utils/catchAsynch');
+const fs = require('fs');
+
+const uploadController = catchAsync(async (req, res) => {
+  const filePath = req.file.path;
+
+  const result = await cloudinary.uploader.upload(filePath,{
+    folder: 'uploads', 
+    resource_type: 'auto',
+  });
+
+   fs.unlinkSync(filePath);
+
+
+  res.status(200).json({
+    message: 'File uploaded successfully',
+    url: result.secure_url,
+    public_id: result.public_id,
+  });
+});
+
+module.exports = uploadController;
