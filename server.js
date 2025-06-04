1ae31fdd61e304b9bc9248989cd14c943089e2db
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-06-04 15:42:06 +0500

    Update and delete functions is Added

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const { connectDB } = require('./Config/db.js');
-const authRoutes = require('./Routes/admin/authRoute.js')
-const clodinaryRoute = require('./Routes/admin/cloudinary.js');
-const adminTimetableRoute = require('./Routes/admin/timeTableRoute.js');
-const publicTimetableRoute = require('./Routes/public/timeTableRoute.js');
-const notificationRoute = require('./Routes/admin/notificationsRoute.js')
-const publicNotificationRoute= require('./Routes/public/notificationsRoute.js')
-const adminNotesRoute = require('./Routes/admin/notesRoute.js')
+// const authRoutes = require('./Routes/admin/authRoute.js')
+// const clodinaryRoute = require('./Routes/admin/cloudinary.js');
+// const adminTimetableRoute = require('./Routes/admin/timeTableRoute.js');
+// const publicTimetableRoute = require('./Routes/public/timeTableRoute.js');
+// const notificationRoute = require('./Routes/admin/notificationsRoute.js')
+// const publicNotificationRoute= require('./Routes/public/notificationsRoute.js')
+// const adminNotesRoute = require('./Routes/admin/notesRoute.js')
 require('dotenv').config();
 connectDB();
 
@@ -21,13 +21,13 @@ app.use(express.json());
 
 
 
-app.use('/api/admin', authRoutes);
-// app.use('/api', clodinaryRoute);
-app.use('/api/admin', adminTimetableRoute);
-app.use('/api/public', publicTimetableRoute);
-app.use('/api/admin', notificationRoute)
-app.use("/api/public", publicNotificationRoute);
-app.use("/api/admin", adminNotesRoute)
+// app.use('/api/admin', authRoutes);
+// // app.use('/api', clodinaryRoute);
+// app.use('/api/admin', adminTimetableRoute);
+// app.use('/api/public', publicTimetableRoute);
+// app.use('/api/admin', notificationRoute)
+// app.use("/api/public", publicNotificationRoute);
+// app.use("/api/admin", adminNotesRoute)
 
 
 
