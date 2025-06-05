40b7cb587f244e26132373e934466b7720c92fd8
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-05 12:05:44 +0500

    Create delete datesheet feature and get datesheet by search query and get all datesheet

--- Routes/admin/dateSheetRoutes.js
+++ Routes/admin/dateSheetRoutes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const verify = require('../../Middleware/verifyAdminMiddleware.js');
 const restrictTo = require('../../Middleware/restrictToMiddleware.js');
 const upload = require('../../Middleware/cloudinary.js');
-const { uploadDateSheet, updateDateSheet } = require('../../Controllers/admin/dateSheetController.js');
+const { uploadDateSheet,
+        updateDateSheet,
+        deleteDateSheet } = require('../../Controllers/admin/dateSheetController.js');
 
 const router = express.Router();
 router.post('/exams/datesheet', verify, restrictTo('admin'), upload.single('file'), uploadDateSheet);
-router.put('/exams/datesheet/:id', verify, restrictTo('admin'), upload.single('file'), updateDateSheet)
+router.put('/exams/datesheet/:id', verify, restrictTo('admin'), upload.single('file'), updateDateSheet);
+router.delete('/exams/datesheet/:id', verify, restrictTo('admin'), deleteDateSheet);
 module.exports = router;
